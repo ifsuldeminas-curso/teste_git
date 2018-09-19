1883d7501757afc917438b4f339f2c134680bcf7
mrbrook <lucasadriano110@hotmail.com>
2018-09-18 22:05:19 -0300

    commit 4

--- code.c
+++ code.c
@@ -3,9 +3,12 @@
 int main(){
 	
 	int x = 3;
-	int y = 5
+	int y = 5;
 	
-	printf("Teste de controle de verção git = %d\n",(x+y));
+	int i = 0;
+	
+	for(i = 0; i<10;i++)
+		printf("Teste de controle de verção git = %d\n",(x+y)*i);
 	
 	return 0;
 }	
