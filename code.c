700c5e02be984976492738359c6def9312a87723
mrbrook <lucasadriano110@hotmail.com>
2018-09-18 22:07:41 -0300

    commit 5

--- code.c
+++ code.c
@@ -8,7 +8,11 @@ int main(){
 	int i = 0;
 	
 	for(i = 0; i<10;i++)
-		printf("Teste de controle de verção git = %d\n",(x+y)*i);
+		printf("Teste de controle de verção git = %d\n",(soma(x,y))*i);
 	
 	return 0;
-}	
+}
+
+int soma(int x,int y){
+	return x+y;
+}		
