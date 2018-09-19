2ecea3b1bc641d6b8ec6ccd8aaf22d5b033c2291
mrbrook <lucasadriano110@hotmail.com>
2018-09-18 22:12:39 -0300

    commit 8

--- code.c
+++ code.c
@@ -2,13 +2,13 @@
 
 int main(){
 	
-	int x = 3;
+	int x = 15;
 	int y = 5;
 	
 	int i = 0;
 	
 	for(i = 0; i<10;i++)
-		printf("Teste de controle de verção git = %d\n",(mult(x,y))*i);
+		printf("Teste de controle de verção git = %d\n",(div(x,y))*i);
 	
 	return 0;
 }
@@ -22,3 +22,7 @@ int subtracao(int x,int y){
 int mult(int x,int y){
 	return x*y;
 }	
+int div(int x,int y){
+	return x/y;
+}	
+
